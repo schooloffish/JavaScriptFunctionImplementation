8b13c695053f5f20d005b63780b0a02ba4c1b582
schooloffish <liuxun@outlook.com>
2020-11-17 10:37:33 +0800

    Create feiInstanceOf.js

---
+++ feiInstanceOf.js
@@ -0,0 +1,55 @@
+function feiInstanceOf(left, right) {
+    const rightType = typeof right;
+    if (!['function', 'object'].includes(rightType)) {
+        throw new Error('right side of "feiInstanceOf" is not an object');
+    }
+
+    if (!right.prototype) {
+        throw new Error('Right-hand side of "instanceof" is not callable');
+    }
+
+    // if left is primitive type, return false
+    const leftType = typeof left;
+    const isPrimitive = [
+        'string',
+        'number',
+        'boolean',
+        'symbol',
+        'bigint',
+    ].includes(leftType);
+    if (isPrimitive) {
+        return false;
+    }
+
+    let leftProto = Object.getPrototypeOf(left);
+    while (leftProto !== null) {
+        if (leftProto === right.prototype) {
+            return true;
+        }
+        leftProto = Object.getPrototypeOf(leftProto);
+    }
+
+    return false;
+}
+
+console.log(feiInstanceOf({}, Object) === {} instanceof Object);
+console.log(feiInstanceOf('', String) === '' instanceof String);
+console.log(feiInstanceOf(1, Number) === 1 instanceof Number);
+console.log(feiInstanceOf(1, String) === 1 instanceof String);
+console.log(feiInstanceOf('', Number) === '' instanceof Number);
+console.log(
+    feiInstanceOf(Symbol.for('test'), Symbol) ===
+        Symbol.for('test') instanceof Symbol
+);
+console.log(feiInstanceOf(1n, BigInt) === 1n instanceof BigInt);
+console.log(feiInstanceOf(true, Boolean) === true instanceof Boolean);
+class A {}
+var a = new A();
+console.log(feiInstanceOf(a, A) === a instanceof A);
+class B extends A {}
+var b = new B();
+console.log(feiInstanceOf(b, A) === b instanceof A);
+class C extends B {}
+var c = new C();
+console.log(feiInstanceOf(c, A) === c instanceof A);
+console.log(feiInstanceOf(c, B) === c instanceof B);
